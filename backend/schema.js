716e6e227d5c80bfbd36719a1267ae7403dbc925
alexanderyefimochkin <alexander.yefimochkin@gmail.com>
2019-01-27 23:17:35 +0000

    schema queries and mutations added

--- backend/schema.js
+++ backend/schema.js
@@ -1,6 +1,7 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  #TYPES#####################################
   scalar Date
 
   enum RoleType {
@@ -14,71 +15,151 @@ const typeDefs = gql`
   }
 
   type Team {    
-    Id: ID!
-    Name: String!    
-    Header: String
-    Description: String
-    CreatedAt: Date!
-    CreatedByUserId: ID!
-    UpdatedAt: Date
-    UpdatedByUserId: ID
-    Members: [Member]
-    JobsAppliedTo: [Job]
-    Tags: [ScoredTag]
+    id: ID!
+    name: String!    
+    header: String
+    description: String
+    createdAt: Date!
+    createdByUserId: ID!
+    updatedAt: Date
+    updatedByUserId: ID
+    members: [Member]
+    jobsAppliedTo: [Job]
+    tags: [ScoredTag]
   }
 
   type Member {
-    User: User!
-    Role: RoleType!
-    NoticePeriod: Int
-    AvailableAt: Date
+    user: User!
+    role: RoleType!
+    noticePeriod: Int
+    availableAt: Date
   }
 
   type User {
-    Id: ID!
-    Available: Boolean!
-    FirstName: String! 
-    LastName: String! 
-    Description: String! 
-    SocialAccounts: [SocialAccount]
-    CreatedAt: Date!
-    CreatedByUserId: ID!
-    UpdatedAt: Date
-    UpdatedByUserId: ID
-    TeamMemberOf: [Team]
-    JobsCreated: [Job]
-    Tags: [ScoredTag]
+    id: ID!
+    available: Boolean!
+    firstName: String! 
+    lastName: String! 
+    description: String! 
+    socialAccounts: [SocialAccount]
+    createdAt: Date!
+    createdByUserId: ID!
+    updatedAt: Date
+    updatedByUserId: ID
+    teamMemberOf: [Team]
+    jobsCreated: [Job]
+    tags: [ScoredTag]
   }
 
   type Job {
-    Id: ID!
-    Name: String!
-    Header: String
-    Description: String
-    Applications: [Team]
-    CreatedAt: Date!
-    CreatedByUserId: ID!
-    UpdatedAt: Date
-    UpdatedByUserId: ID
-    Tags: [Tag]
+    id: ID!
+    name: String!
+    header: String
+    description: String
+    applications: [Team]
+    createdAt: Date!
+    createdByUserId: ID!
+    updatedAt: Date
+    updatedByUserId: ID
+    tags: [Tag]
   }
 
   type SocialAccount {
-    ExternalId: String!
-    Type: SocialAccountType!
+    externalId: String!
+    type: SocialAccountType!
   }
  
   type ScoredTag {
-    Name: String!
-    Score: Int
+    name: String!
+    score: Int
   }
 
   type Tag {
-    Name: String!
+    name: String!
   }
 
-  type Query {
-    allUsers: [User]
+  #QUERIES#####################################
+
+  #based on token
+  query {
+    me: User
+  }
+
+  query Team($id: Int) {
+    team(id: $id): Team
+  }
+
+  query Job($id: Int) {
+    job(id: $id): Job
+  }
+
+  query MatchingTags ($search: String, $first: Int, $cursor: String) {
+    matches(search: $search, first: $first, cursor: $cursor): {
+      totalCount
+      edges {
+        node: Tag
+        cursor
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+
+  query MatchingJobs ($tags: Tag[], $first: Int, $cursor: String) {
+    matches(tags: $tags, first: $first, cursor: $cursor): {
+      totalCount
+      edges {
+        node: Job
+        cursor
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+
+  query MatchingTeamSummaries ($tags: Tag[], $first: Int, $cursor: String) {
+    matches(tags: $tags, first: $first, cursor: $cursor): {
+      totalCount
+      edges {
+        node: TeamSummary
+        cursor
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+
+  fragment TeamSummary on Team {
+    id
+    name  
+    header
+    description
+    tags
+  }
+
+  #MUTATIONS###################################
+  
+  # the only upsertable user is the authenticated one 
+  mutation UpsertUser ($user: User){
+    upsert(user: $user): User
+  }
+
+  mutation UpsertTeam($team: Team) {
+    upsert(team: $team): Team
+  }
+
+  mutation UpsertJob($job: Job) {
+    upsert(job: $job): Job
+  }
+
+  mutation UpsertUser($user: User) {
+    upsert(user: $user): User
   }
 `;
 
