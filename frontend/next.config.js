70fda72a3dc569971506733c6dcdd33c77ecc97a
alexanderyefimochkin <alexander.yefimochkin@gmail.com>
2019-02-07 23:26:21 +0000

    serverRuntimeConfig and publicRuntimeConfig examples added

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,5 +2,12 @@ module.exports = {
   webpack: function(config) {
     config.externals.push('fs');
     return config;
+  },
+  serverRuntimeConfig: { // Will only be available on the server side
+    mySecret: 'secret'
+  },
+  publicRuntimeConfig: { // Will be available on both server and client
+    staticFolder: '/static',
+    mySecret: process.env.MY_SECRET // Pass through env variables
   }
 }
